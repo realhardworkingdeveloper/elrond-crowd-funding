88c899d0825cd6572ac8bb4a5d545d99a40d2ed4
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-04-02 10:50:58 -0700

    create init functionality and test case of mandos

---
+++ src/crowdfunding.rs
@@ -0,0 +1,16 @@
+#![no_std]
+
+elrond_wasm::imports!();
+
+#[elrond_wasm::contract]
+pub trait Crowdfunding {
+    
+    #[view]
+    #[storage_mapper("target")]
+    fn target(&self) -> SingleValueMapper<BigUint>;
+
+    #[init]
+    fn init(&self, target: BigUint) {
+        self.target().set(&target);
+    }
+}
